d66f9742febb6d74a681b145781dc792f6a03629
Jake Marwil <rjm02006@gmail.com>
2015-02-20 11:12:29 -0700

    reconfigure routes to add resources for statuses and likes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'users#index'
 
-  resources :user
+  resources :user do
+    resources :status do
+      resources :like
+    end
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
